772ce4696ae7dfdb22cceeb393baf8bc92e5ee97
YuraKhoma <yurahoma2014@gmail.com>
2021-07-20 13:24:55 +0300

    Initial check-in of module Task

---
+++ Task/ButtonModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Task
+{
+    class ButtonModel : Button
+    {
+        public int Number { get; set; }
+
+        public override string ToString()
+        {
+            return Number.ToString();
+        }
+    }
+}
---
+++ Task/ButtonViewModel.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Task
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ButtonViewModel : ContentView, INotifyPropertyChanged
+    {
+        
+        readonly IList<ButtonModel> source;
+
+        ObservableCollection<ButtonModel> Buttons { get; set; }
+
+        public ButtonViewModel()
+        {
+            source = new List<ButtonModel>();
+
+            CreateButtonCollection();
+        }
+
+        void CreateButtonCollection()
+        {
+            source.Add(new ButtonModel
+            {
+                Number = 1
+            });
+            source.Add(new ButtonModel
+            {
+                Number = 2
+            });
+            source.Add(new ButtonModel
+            {
+                Number = 3
+            });
+            source.Add(new ButtonModel
+            {
+                Number = 4
+            });
+            source.Add(new ButtonModel
+            {
+                Number = 5
+            });
+            source.Add(new ButtonModel
+            {
+                Number = 6
+            });
+            source.Add(new ButtonModel
+            {
+                Number = 7
+            });
+            source.Add(new ButtonModel
+            {
+                Number = 8
+            });
+            source.Add(new ButtonModel
+            {
+                Number = 9
+            });
+            source.Add(new ButtonModel
+            {
+                Number = 10
+            });
+            Buttons = new ObservableCollection<ButtonModel>(source);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
